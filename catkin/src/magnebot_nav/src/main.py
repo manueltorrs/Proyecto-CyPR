f57d4ee2bc4a41cad507b6ae7897b16dd81bb7c0
rlkandela <rlkandela@gmail.com>
2021-05-17 23:10:58 +0200

    no funciona, pero tengo que formatear y no quiero perderlo

--- catkin/src/magnebot_nav/src/main.py
+++ catkin/src/magnebot_nav/src/main.py
@@ -1,10 +1,36 @@
 #!/bin/env python3
 from magnebot import Magnebot
+import rospy
+# from std_msgs.msg import PointCloud2
+from sensor_msgs.point_cloud2 import PointCloud2
+# from std_msgs.msg import Image as SensorImage
+# from PIL import Image as PILImage
 
-m = Magnebot(launch_build=True)
+rospy.init_node("Nodo_1")
 
-m.init_floorplan_scene("2b", 0, 1)
+pc_pub = rospy.Publisher("/mgbot/pointcloud", PointCloud2, queue_size=1)
+_ = PointCloud2()
 
-m.state.save_images("test_image_output")
+m = Magnebot(
+    launch_build=True,
+    skip_frames=100)
+
+# rgb_pub = rospy.Publisher("/mgbot/rgb_img", SensorImage)
+
+init = 0
+
+m.init_floorplan_scene("2b", 0, 3)
+while True:
+    m.move_by(0.01)
+    pointcloud = m.state.get_point_cloud()
+    msg = PointCloud2()
+    msg.data = pointcloud
+    pc_pub.publish(msg)
+    if not init:
+        init = 1
+        print("starting to pub")
+    # rgb_img = m.state.get_pil_images()
+    # depth_img = m.state.get_depth_values()
+    m.state.save_images("test_image_output")
 
 m.end()
