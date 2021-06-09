d24abb25d626f7b0331dd5bd4b6b7ea308bfb06a
OutboardTech76 <fme11@protonmail.com>
2021-06-09 18:05:52 +0200

    Use Open3D to visualize Point Cloud generated by Magnebot

--- catkin/src/magnebot_nav/src/main.py
+++ catkin/src/magnebot_nav/src/main.py
@@ -54,12 +54,14 @@ if __name__ == "__main__":
     # m = Magnebot(launch_build=True, screen_width=1920, screen_height=1080)
     m = Magnebot(launch_build=True)
 
-    m.init_floorplan_scene("2b", 0, 3)
+    m.init_floorplan_scene("2b", 0, 2)
     # ipdb.set_trace()
-    img = m.state.get_point_cloud()
-    print("PC: {}".format(img))
-    print("Type: {}".format(type(img)))
-    print("Shape: {}".format(img.shape))
+    points = m.state.get_point_cloud()
+    images = m.state.get_pil_images()
+    print("PC: {}".format(points))
+    print("Type: {}".format(type(points)))
+    print("Shape: {}".format(points.shape))
+    m.add_camera(position={"x": 1.43, "y": 1.87, "z": 0.77}, look_at=True, follow=True)
 
     done = False
     status = False
@@ -72,41 +74,62 @@ if __name__ == "__main__":
     # m.listen(key="B", function=move(m))
     ids = 0 
     commands = []
-    for joint in m.magnebot_static.joints.values():
-        if joint.name == "column":
-            ids = joint.id
+    # for joint in m.magnebot_static.joints.values():
+        # if joint.name == "column":
+            # ids = joint.id
+    # commands.append({"$type": "set_revolute_target",
+                     # "joint_id": ids,
+                     # "target": 124})
+    # m.communicate(commands)
+    # # Wait for a bit
+    # for i in range(100):
+        # m.communicate([])
+    
 
     # for wheel in m.magnebot_static.wheels:
         # commands.append({"$type": "set_revolute_target",
                          # "joint_id": m.magnebot_static.wheels[wheel],
                          # "target": 1200})
 
-    pcd = o3d.geometry.PointCloud()
+    # Change points size from (3, 256, 256) to (n, 3) to visualize point cloud
+    # Where n is 256x256 and 3 each dimension (x, y, z)
+    xyz = np.zeros((np.size(points[0,:,:]), 3))
+    xyz[:, 0] = np.reshape(points[0, :, :], -1)
+    xyz[:, 1] = np.reshape(points[1, :, :], -1)
+    xyz[:, 2] = np.reshape(points[2, :, :], -1)
 
-    countBase = 100
-    countRot = 20
-    m.listen(key='W', commands=[{"$type": "set_revolute_target",
-                         "joint_id": m.magnebot_static.wheels[wheel],
-                         "target": (countBase+1)} for wheel in m.magnebot_static.wheels],
-             events=["press", "hold"])
-    m.listen(key="R", commands={"$type": "set_revolute_target",
-                                "joint_id": ids,
-                                "target": (countRot+10)}, events=["press", "hold"])
-    m.listen(key="E", commands={"$type": "set_revolute_target",
-                                "joint_id": ids,
-                                "target": (countRot-10)}, events=["press", "hold"])
-    m.listen(key='S', function=m.reset_position)
-        # print("Joint name: {}".format(joint))
+    ipdb.set_trace()
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(xyz)
+
+    o3d.visualization.draw([pcd])
+     
+     
+     
+    # countBase = 100
+    # countRot = 20
+    # m.listen(key='W', commands=[{"$type": "set_revolute_target",
+                         # "joint_id": m.magnebot_static.wheels[wheel],
+                         # "target": (countBase+1)} for wheel in m.magnebot_static.wheels],
+             # events=["press", "hold"])
     # m.listen(key="R", commands={"$type": "set_revolute_target",
-                     # "joint_id": m.magnebot_static.joints[],
-                     # "target": 124}, events=["press", "hold"])
-    # m.listen(key="w", function=m.move_by(0.001))
-    # m.listen(key="d", function=m.turn_by(0.001))
-    while not done:
-        m.communicate([])
-        # m.turn_by(0.001)
-        # m.move_by(0.01)
-    # m.communicate(commands)
+                                # "joint_id": ids,
+                                # "target": (countRot+10)}, events=["press", "hold"])
+    # m.listen(key="E", commands={"$type": "set_revolute_target",
+                                # "joint_id": ids,
+                                # "target": (countRot-10)}, events=["press", "hold"])
+    # m.listen(key='S', function=m.reset_position)
+        # # print("Joint name: {}".format(joint))
+    # # m.listen(key="R", commands={"$type": "set_revolute_target",
+                     # # "joint_id": m.magnebot_static.joints[],
+                     # # "target": 124}, events=["press", "hold"])
+    # # m.listen(key="w", function=m.move_by(0.001))
+    # # m.listen(key="d", function=m.turn_by(0.001))
+    # while not done:
+        # m.communicate([])
+        # # m.turn_by(0.001)
+        # # m.move_by(0.01)
+    # # m.communicate(commands)
 
     m.state.save_images("test_image_output")
 
